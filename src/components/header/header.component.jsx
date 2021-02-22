3bfc59d260dedca7312327604c0bc562ef6e177f
kamal patel <kamal.patel30@hotmail.com>
2021-02-22 12:48:52 +0530

    removed crwn logo and added name logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 import "./header.styles.scss";
 import CartIcon from "../cart-icon/cart-icon.component";
